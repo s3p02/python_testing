244745ffa0d3eddc283186029953d2d2db320920
Sree Sai Sharan <SreeSaiSharan.Pantangi@charter.com>
2018-02-22 15:43:09 -0700

    tools added

---
+++ tools/test_sample.py
@@ -0,0 +1,9 @@
+#FILE_BEGINS_HERE
+def add_one(input_value):
+    return input_value + 1
+def test_answer():
+    assert add_one(1)==2
+    assert add_one(2)==2
+#if __name__=='__main__':
+#    main()
+#END_OF_FILE
